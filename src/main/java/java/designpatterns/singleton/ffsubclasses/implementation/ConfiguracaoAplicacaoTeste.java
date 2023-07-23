6565371db9f067041c9d9bdec6b61fec5519aa43
Rafiere <rafapgbr@gmail.com>
2023-07-23 12:02:05 -0300

    feat: add singleton with subclasses example

---
+++ src/main/java/java/designpatterns/singleton/ffsubclasses/implementation/ConfiguracaoAplicacaoTeste.java
@@ -0,0 +1,10 @@
+package java.designpatterns.singleton.ffsubclasses.implementation;
+
+public class ConfiguracaoAplicacaoTeste extends ConfiguracaoAplicacao {
+
+	protected ConfiguracaoAplicacaoTeste(){
+		this.nomeAplicacao = "Singleton Teste";
+		this.versao = "3.0";
+		this.cor = "Vermelho";
+	}
+}
