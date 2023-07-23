b7bd97e4281a132c10f0eb89833393192a8489e5
Rafiere <rafapgbr@gmail.com>
2023-07-22 23:23:29 -0300

    feat: add strategy pattern first example

---
+++ src/main/java/java/designpatterns/strategy/bbexemplocomdados/ExemploComDados.java
@@ -0,0 +1,10 @@
+package java.designpatterns.strategy.bbexemplocomdados;
+
+public class ExemploComDados {
+
+	/* Nesse exemplo, o formato com dados é o formato mais padrão. Esse é
+	* o formato que temos um menor acoplamento, pois a implementação da
+	* estratégia não conhece a compra, apenas o dado que é importante para
+	* ela, que é o valor. Nesse modelo, temos um menor acoplamento, porém, a
+	* estratégia não consegue manipular a compra. */
+}
