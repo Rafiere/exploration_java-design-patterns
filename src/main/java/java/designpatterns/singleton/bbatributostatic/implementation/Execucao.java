8064b8e3da10fb9af138620ac5c7970cddf009ad
Rafiere <rafapgbr@gmail.com>
2023-07-23 12:00:50 -0300

    feat: add singleton with a static attribute example

---
+++ src/main/java/java/designpatterns/singleton/bbatributostatic/implementation/Execucao.java
@@ -0,0 +1,9 @@
+package java.designpatterns.singleton.bbatributostatic.implementation;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+
+		Runtime runtime = Runtime.getRuntime();
+	}
+}
