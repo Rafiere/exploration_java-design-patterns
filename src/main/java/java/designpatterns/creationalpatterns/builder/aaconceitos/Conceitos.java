98a75e027e4ecc56ce56b853922ecb5f3862a089
Rafiere <rafapgbr@gmail.com>
2023-07-24 21:10:23 -0300

    feat: add builder concepts

---
+++ src/main/java/java/designpatterns/creationalpatterns/builder/aaconceitos/Conceitos.java
@@ -0,0 +1,8 @@
+package java.designpatterns.creationalpatterns.builder.aaconceitos;
+
+public class Conceitos {
+
+	/* Esse é um padrão de projeto utilizado quando queremos criar objetos
+	* que são enormes, recebendo muitos parâmetros, ou seja, objetos que tenham
+	* muitas opções de configuração. */
+}
