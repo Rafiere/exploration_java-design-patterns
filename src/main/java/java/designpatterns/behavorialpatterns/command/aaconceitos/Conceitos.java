7cbfb233486adca91dfbd54790a79c724837f0b6
Rafiere <rafapgbr@gmail.com>
2023-07-25 20:47:57 -0300

    feat: add concepts in command

--- src/main/java/java/designpatterns/behavorialpatterns/command/aaconceitos/Conceitos.java
+++ src/main/java/java/designpatterns/behavorialpatterns/command/aaconceitos/Conceitos.java
@@ -2,4 +2,14 @@ package java.designpatterns.behavorialpatterns.command.aaconceitos;
 
 public class Conceitos {
 
+	/* Os exemplos apresentados foram retirados do artigo do Baeldung sobre
+	* o padrão "command". */
+
+	/* Ele é um padrão de design que permite o encapsulamento de todos os
+	* dados que são requeridos para performar uma determinada ação em um
+	* único objeto. */
+
+	/* Ele permite o desacoplamento de objetos que produzem os comandos de
+	* seus consumidores. Por isso que ele é conhecido como um padrão
+	* "producer-consumer". */
 }
