d3af20bdafb85b10fce4c758aea5d14e2df931fc
Rafiere <rafapgbr@gmail.com>
2023-07-27 20:53:58 -0300

    feat: add state pattern main concepts

---
+++ src/main/java/java/designpatterns/behavorialpatterns/state/State.java
@@ -0,0 +1,7 @@
+package java.designpatterns.behavorialpatterns.state;
+
+public class State {
+
+	/* O padrão de projeto "State", basicamente, permite que um objeto altere o seu
+	* comportamento quando o seu estado interno alterar. */
+}
