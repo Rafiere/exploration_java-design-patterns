c7276815d7976f2e91273867feac36bb003d0b40
Rafiere <rafapgbr@gmail.com>
2023-07-26 23:35:34 -0300

    feat: add iterator pattern main class

---
+++ src/main/java/java/designpatterns/behavorialpatterns/iterator/Iterator.java
@@ -0,0 +1,10 @@
+package java.designpatterns.behavorialpatterns.iterator;
+
+public class Iterator {
+
+	/* Esse padrão serve para encapsularmos uma determinada estrutura de dados e
+	* utilizarmos ela sem expor a implementação. */
+
+	/* Novos algoritmos podem ser utilizados, pois estamos dependendo da
+	* interface, e não da implementação. */
+}
