6e14c436f4d7be840a610780b007f64a20f2b748
Rafiere <rafapgbr@gmail.com>
2023-07-27 20:53:48 -0300

    feat: add state pattern concepts

---
+++ src/main/java/java/designpatterns/behavorialpatterns/state/aaconceitos/Conceitos.java
@@ -0,0 +1,9 @@
+package java.designpatterns.behavorialpatterns.state.aaconceitos;
+
+public class Conceitos {
+
+	/* O padrão de design "state" permite que um objeto altere o seu comportamento
+	* quando um estado interno for alterado. */
+
+	/* O "context" é o objeto que contém o "State". */
+}
