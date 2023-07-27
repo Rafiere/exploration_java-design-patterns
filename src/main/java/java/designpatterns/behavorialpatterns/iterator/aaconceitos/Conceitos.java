316699db96140ffd75ad9644c53c921eaa3d1061
Rafiere <rafapgbr@gmail.com>
2023-07-26 23:33:58 -0300

    feat: add iterator pattern

---
+++ src/main/java/java/designpatterns/behavorialpatterns/iterator/aaconceitos/Conceitos.java
@@ -0,0 +1,15 @@
+package java.designpatterns.behavorialpatterns.iterator.aaconceitos;
+
+public class Conceitos {
+
+	/* Se temos um grafo e queremos iterar sobre os elementos desse grafo, mas sem
+	* expor a implementação dele, ou seja, ele pode ser uma binary search tree, uma red black
+	* search tree e etc. */
+
+	/* O principal objetivo desse padrão é extrair a implementação de uma coleção em um
+	* objeto separado, que é chamado de iterator. */
+
+	/* Muitos iterators podem estar na mesma coleção ao mesmo tempo. */
+
+	/* Todos os iterators devem implementar a mesma interface */
+}
