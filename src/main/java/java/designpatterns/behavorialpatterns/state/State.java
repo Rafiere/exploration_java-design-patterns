dbc1c6c92de7fb00171772cc4e71f270c0f4f736
Rafiere <rafapgbr@gmail.com>
2023-07-27 21:34:27 -0300

    feat: add comment in state pattern

--- src/main/java/java/designpatterns/behavorialpatterns/state/State.java
+++ src/main/java/java/designpatterns/behavorialpatterns/state/State.java
@@ -4,4 +4,8 @@ public class State {
 
 	/* O padrão de projeto "State", basicamente, permite que um objeto altere o seu
 	* comportamento quando o seu estado interno alterar. */
+
+	/* Podemos usar esse padrão quando temos uma classe com inúmeros condicionais que
+	* alteram o comportamento da classe de acordo com os valores que estão nos campos
+	* de uma classe. */
 }
