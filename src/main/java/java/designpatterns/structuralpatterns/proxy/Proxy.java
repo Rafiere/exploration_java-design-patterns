3e68089f1068833d3be813baa46012bc35d69482
Rafiere <rafapgbr@gmail.com>
2023-07-28 23:04:12 -0300

    feat: add Proxy pattern concepts

---
+++ src/main/java/java/designpatterns/structuralpatterns/proxy/Proxy.java
@@ -0,0 +1,15 @@
+package java.designpatterns.structuralpatterns.proxy;
+
+public class Proxy {
+
+	/* No padrão "proxy", temos o "RealSubject" e o
+	* "Proxy". */
+
+	/* Ele é uma entidade que recebe uma chamada, faz diversas coisas
+	* com ela e retorna para quem chamou. */
+
+	/* Ele é um padrão muito flexível e abre muitas possibilidades. */
+
+	/* Uma das vantagens desse padrão é que não precisamos mexer nas implementações que
+	* já estão prontas. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/proxy/aaconceitos/Conceitos.java
@@ -0,0 +1,5 @@
+package java.designpatterns.structuralpatterns.proxy.aaconceitos;
+
+public class Conceitos {
+
+}
