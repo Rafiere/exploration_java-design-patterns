fc78d3e287db32b2b6bb19fa846f9edacc1d9834
Rafiere <rafapgbr@gmail.com>
2023-07-29 20:54:37 -0300

    feat: add adapter method concepts

---
+++ src/main/java/java/designpatterns/structuralpatterns/adapter/Adapter.java
@@ -0,0 +1,11 @@
+package java.designpatterns.structuralpatterns.adapter;
+
+public class Adapter {
+
+	/* O padrão "Adapter" permite a colaboração entre objetos que possuem
+	* interfaces compatíveis. */
+
+	/* O padrão adapter possui o "client", o "adapter" e o "adaptee". */
+
+	/* O "Client" é quem está usando o que foi implementado. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/adapter/aaconceitos/Conceitos.java
@@ -0,0 +1,7 @@
+package java.designpatterns.structuralpatterns.adapter.aaconceitos;
+
+public class Conceitos {
+
+	/* Um "adapter" é um objeto especial que converte a interface de um objeto em
+	* uma interface que outro objeto possa entender. */
+}
