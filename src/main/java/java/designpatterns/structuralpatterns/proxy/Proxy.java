4fb1eb67fe0c95751959bc09650af6a571f5d0e0
Rafiere <rafapgbr@gmail.com>
2023-07-29 20:08:29 -0300

    feat: add Proxy pattern concept

--- src/main/java/java/designpatterns/structuralpatterns/proxy/Proxy.java
+++ src/main/java/java/designpatterns/structuralpatterns/proxy/Proxy.java
@@ -12,4 +12,26 @@ public class Proxy {
 
 	/* Uma das vantagens desse padrão é que não precisamos mexer nas implementações que
 	* já estão prontas. */
+
+	/* Exemplos reais: */
+
+	/* O "@Autowired" utiliza o padrão proxy. A instância que é injetada não é uma
+	* instância da classe, e sim uma subclasse, que é um proxy, assim, é injetado um
+	* proxy. Isso acontece pois alguns frameworks fornecem um interceptador, por
+	* exemplo. Para esse interceptador ser chamado, temos que fazer uma chamada para a
+	* implementação do proxy criado do framework, para, após isso, chamar a implementação
+	* real do método. */
+
+	/* O pacote RMI do Java também utiliza isso. */
+
+	/* Vantagens: */
+
+	/* 1. Conseguimos aplicar sem mexer no cliente e na implementação original. */
+
+	/* Desvantagens: */
+
+	/* 1. Estamos criando mais classes. */
+
+	/* 2. É importante entendermos que cada proxy inserido no caminho acaba tornando as operações
+	* muito mais lentas. */
 }
