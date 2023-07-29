0060a82a8572547ad93ff553d8c18076fad6c228
Rafiere <rafapgbr@gmail.com>
2023-07-28 21:20:02 -0300

    feat: add flyweight concepts

---
+++ src/main/java/java/designpatterns/structuralpatterns/flyweight/Flyweight.java
@@ -0,0 +1,16 @@
+package java.designpatterns.structuralpatterns.flyweight;
+
+public class Flyweight {
+
+	/* Esse é um padrão de projeto com um foco muito grande na economia de memória. */
+
+	/* Um caractere possui 8 bits. Quando transformamos ele em uma classe, ele passará
+	* a ter, no mínimo, 125 bytes. Isso significa, no mínimo, que multiplicamos o tamanho
+	* dele por 125. */
+
+	/* Esse padrão foi usado para impedir que o editor use gigabytes de memória apenas
+	* para abrir um livro. */
+
+	/* Esse é um padrão para otimização. Só devemos utilizar esse padrão quando
+	* necessário. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/flyweight/aaconceitos/Conceitos.java
@@ -0,0 +1,9 @@
+package java.designpatterns.structuralpatterns.flyweight.aaconceitos;
+
+public class Conceitos {
+
+	/* Esse padrão recomenda que não armazenamos estados extrínsicos dentro do objeto. Ao
+	* invés disso, devemos passar esse estado para métodos específicos. Apenas os estados
+	* intrínsicos devem permanecer dentro do objeto, permitindo o reuso em diferentes
+	* contextos. */
+}
