05fdc7972fa71c228c20aa4ef9e80159ec83cb4f
Rafiere <rafapgbr@gmail.com>
2023-07-30 11:51:01 -0300

    feat: add bridge pattern concepts

---
+++ src/main/java/java/designpatterns/structuralpatterns/bridge/Bridge.java
@@ -0,0 +1,21 @@
+package java.designpatterns.structuralpatterns.bridge;
+
+public class Bridge {
+
+	/* Esse padrão de projeto permite que dividamos uma classe única que
+	* tem múltiplas variantes da mesma funcionalidade em duas hierarquias, que
+	* são as abstrações e as implementações. */
+
+	/* Com isso, o código do cliente não será exposto para os detalhes
+	* de implementação. */
+
+	/* Dessa forma, também vamos introduzir novas abstrações e
+	* implementações, tornando fácil a alteração delas em runtime. */
+
+	/* Quando usar: */
+
+	/* 1. Devemos usar esse padrão quando queremos organizar uma classe
+	* monolítica que tem muitas variantes da mesma funcionalidade, por
+	* exemplo, se a classe pode trabalhar com vários servidores de
+	* banco de dados. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/bridge/aaconceitos/Conceitos.java
@@ -0,0 +1,8 @@
+package java.designpatterns.structuralpatterns.bridge.aaconceitos;
+
+public class Conceitos {
+
+	/* Basicamente, esse padrão de projeto divide a lógica de negócios
+	* de uma grande classe em diferentes hierarquias de classes, que
+	* podem ser desenvolvidas de forma independente. */
+}
