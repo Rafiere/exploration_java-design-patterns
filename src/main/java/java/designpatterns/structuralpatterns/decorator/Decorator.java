dcbec5c458ad770d89e6b269ef52c285ba374f4d
Rafiere <rafapgbr@gmail.com>
2023-07-29 23:29:07 -0300

    feat: add decorator pattern concepts.

---
+++ src/main/java/java/designpatterns/structuralpatterns/decorator/Decorator.java
@@ -0,0 +1,9 @@
+package java.designpatterns.structuralpatterns.decorator;
+
+public class Decorator {
+
+
+	/* A principal ideia do Decorator é conseguirmos adicionar uma funcionalidade sob uma
+	* classe que já temos sem utilizar herança e conseguir acrescentar funcionalidades
+	* em uma classe. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/decorator/aaconceitos/Conceitos.java
@@ -0,0 +1,5 @@
+package java.designpatterns.structuralpatterns.decorator.aaconceitos;
+
+public class Conceitos {
+
+}
