ee6a020e48019babd2123808da8ac763b0954585
Rafiere <rafapgbr@gmail.com>
2023-07-30 14:16:31 -0300

    feat: add composite patterns concept

---
+++ src/main/java/java/designpatterns/structuralpatterns/composite/Composite.java
@@ -0,0 +1,11 @@
+package java.designpatterns.structuralpatterns.composite;
+
+public class Composite {
+
+	/* Esse padrão só faz sentido quando o modelo "core" da aplicação pode
+	* ser representado como uma árvore. */
+
+	/* Basicamente, esse padrão realiza a composição de objetos em estruturas
+	* de árvores e então trabalha com essas estruturas como se elas fossem
+	* objetos individuais. */
+}
---
+++ src/main/java/java/designpatterns/structuralpatterns/composite/aaconceitos/Conceitos.java
@@ -0,0 +1,8 @@
+package java.designpatterns.structuralpatterns.composite.aaconceitos;
+
+public class Conceitos {
+
+	/* Esse padrão permite realizarmos a composição de objetos em
+	* estruturas de árvores, e trabalhar com elas como se fossem um
+	* objeto singular. */
+}
