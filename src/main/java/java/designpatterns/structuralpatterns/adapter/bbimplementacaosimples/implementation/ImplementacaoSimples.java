85da61daae096f92e610788eab6fc5275e3c6061
Rafiere <rafapgbr@gmail.com>
2023-07-29 21:09:04 -0300

    refactor! delete unused class

--- src/main/java/java/designpatterns/structuralpatterns/adapter/bbimplementacaosimples/implementation/ImplementacaoSimples.java
+++
@@ -1,7 +0,0 @@
-package java.designpatterns.structuralpatterns.adapter.bbimplementacaosimples.implementation;
-
-public class ImplementacaoSimples {
-
-	/* Esse é um exemplo mais simples de implementação do padrão "Adapter". Ele não
-	* está escrito no livro do GoF. */
-}
