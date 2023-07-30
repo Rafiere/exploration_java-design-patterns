f6a7c01a9723a10c1241a60ecabf51facdd5fbe5
Rafiere <rafapgbr@gmail.com>
2023-07-29 21:09:20 -0300

    feat: add comments

--- src/main/java/java/designpatterns/structuralpatterns/adapter/ccimplementacaocomcomposicao/ImplementacaoComComposicao.java
+++ src/main/java/java/designpatterns/structuralpatterns/adapter/ccimplementacaocomcomposicao/ImplementacaoComComposicao.java
@@ -2,4 +2,11 @@ package java.designpatterns.structuralpatterns.adapter.ccimplementacaocomcomposi
 
 public class ImplementacaoComComposicao {
 
+	/* Esse exemplo é como o design pattern "Adapter" é explicado no livro do
+	* GoF. */
+
+	/* Esse padrão é muito utilizado internamente em frameworks ou bibliotecas. Nem
+	* sempre vamos usar esses padrões dentro de nosso projeto de negócio. */
+
+	/* Nesse exemplo, */
 }
